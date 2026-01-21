616006de7fb26266c0210b7592756bc73681df86
John Jenkins <johnljenkins@hotmail.com>
2026-01-21 00:38:42 +0000

    chore: csb

--- packages/next-app/next.config.ts
+++ packages/next-app/next.config.ts
@@ -3,6 +3,7 @@ import path from 'path';
 
 const nextConfig: NextConfig = {
   outputFileTracingRoot: path.join(__dirname, '../..'),
+  allowedDevOrigins: ['*.csb.app'],
   webpack: (config, { dev }) => {
     if (dev) {
       config.watchOptions = {
